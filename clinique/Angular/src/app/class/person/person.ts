5a5845d55696ab20223bfbb9436e1dcdf5ad3db2
imanebenhdm <imane.benchouk@gmail.com>
2022-01-16 14:19:46 +0000

    update

---
+++ clinique/Angular/src/app/class/person/person.ts
@@ -0,0 +1,12 @@
+export class Person {
+    nom:string;
+    prenom:string;
+    genre : Generator;
+    constructor(n:string , p:string , g:genre){
+        this.nom = n;
+        this.prenom = p;
+        this.genre = g;
+    }
+
+}
+
