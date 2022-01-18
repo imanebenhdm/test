10b0c204857cd2b47431964c5c98856269851190
imanebenhdm <imane.benchouk@gmail.com>
2022-01-18 18:45:38 +0000

    update

--- clinique/Angular/src/app/class/person/person.ts
+++ clinique/Angular/src/app/class/person/person.ts
@@ -1,12 +1,30 @@
 export class Person {
     nom:string;
     prenom:string;
-    genre : Generator;
+    genre : genre;
     constructor(n:string , p:string , g:genre){
         this.nom = n;
         this.prenom = p;
         this.genre = g;
     }
+    getNomComplet(){
+        return this.prenom + " " + this.nom;
+    }
+    getNomFormate(){
+        let formatName = "";
+        switch (this.genre){
+            case genre.mal:
+                formatName += "M" + "." + this.prenom.charAt(0).toUpperCase + this.nom.toUpperCase;
+            break;
+            case genre.femelle:
+                formatName += "Mme" + "." + this.prenom.charAt(0).toUpperCase + this.nom.toUpperCase;
+            break;
 
+        }
+    }
 }
 
+enum genre{
+    mal , 
+    femelle
+}
