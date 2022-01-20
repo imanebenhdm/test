cf15467017c467cdb0ac770fee5fc50ed42b5a96
imanebenhdm <imane.benchouk@gmail.com>
2022-01-20 20:07:08 +0000

    update

--- clinique/Angular/src/main.ts
+++ clinique/Angular/src/main.ts
@@ -12,17 +12,17 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-  
+ //********************************************************************* */ 
 let a = new Person("ben" , "tad" , 1);
 console.log(a.getNomComplet("Mme "));
 document.getElementById("essaie")!.innerHTML = a.getNomFormate();
-
+//********************************************************************** */
 let b = new DbManagerService();
 let c = [];
 for (let patient of b.patients){
-  console.log('salut');
+  document.getElementById("essaie1")!.innerHTML = "bonjour mimi";
   console.log('Genre: '+ patient.genre);
  // patient.genre + " " + patient.nom + " " + patient.prenom + " " + patient.anneeNaissance;
-  document.getElementById("essaie1")!.innerHTML += b.getAllPatient();
+  //document.getElementById("essaie1")!.innerHTML += b.getAllPatient();
   
 }
