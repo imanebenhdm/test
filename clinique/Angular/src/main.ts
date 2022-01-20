140fed1dd7f905456e08bae3de964eefa0885a2b
imanebenhdm <imane.benchouk@gmail.com>
2022-01-20 17:57:46 +0000

    update

--- clinique/Angular/src/main.ts
+++ clinique/Angular/src/main.ts
@@ -3,10 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import { Person , genre } from "./app/class/person/person"
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+  
+let a = new Person("ben" , "tad" , 0);
+console.log(a.getNomComplet("Mme "));
+document.getElementById("essaie")!.innerHTML = a.getNomFormate();
